8fa05776bccd02cfe8a7d6a694b4ef068a240354
kouwa stephane <kksasteph888@gmail.com>
2024-03-01 14:30:43 +0100

    Maj

--- src/HabeukProfileApplyConfig.php
+++ src/HabeukProfileApplyConfig.php
@@ -49,21 +49,33 @@ class HabeukProfileApplyConfig {
   protected static function instaLLNewTheme($themename) {
     /**
      *
-     * @var \Drupal\Core\Extension\ThemeInstaller $themeInstaller
+     * @var \Drupal\Core\Extension\ThemeExtensionList $ExtLitThemes
      */
-    $themeInstaller = \Drupal::service("theme_installer");
-    $listThemesInstalled = $listThemesInstalled = \Drupal::config("core.extension")->get('theme');
-    if (empty($listThemesInstalled[$themename])) {
-      $theme_list = [
-        $themename => $themename
-      ];
-      if ($themeInstaller->install($theme_list))
-        \Drupal::messenger()->addStatus("Le theme '$themename' a été installé ");
-      else
-        \Drupal::messenger()->addError("Le theme '$themename' n'a pa pu etre installé ");
+    $ExtLitThemes = \Drupal::service('extension.list.theme');
+    $ExtLitThemes->reset();
+    $listThemeVisible = $ExtLitThemes->getList();
+    if (!empty($listThemeVisible[$themename])) {
+      /**
+       *
+       * @var \Drupal\Core\Extension\ThemeInstaller $themeInstaller
+       */
+      $themeInstaller = \Drupal::service("theme_installer");
+      $listThemesInstalled = $listThemesInstalled = \Drupal::config("core.extension")->get('theme');
+      if (empty($listThemesInstalled[$themename])) {
+        $theme_list = [
+          $themename => $themename
+        ];
+        if ($themeInstaller->install($theme_list))
+          \Drupal::messenger()->addStatus("Le theme '$themename' a été installé ");
+        else
+          \Drupal::messenger()->addError("Le theme '$themename' n'a pa pu etre installé ");
+      }
+      else {
+        \Drupal::messenger()->addStatus("Le theme '$themename' est deja installé");
+      }
     }
     else {
-      \Drupal::messenger()->addStatus("Le theme '$themename' est deja installé");
+      \Drupal::messenger()->addStatus("Le theme '$themename' n'est pas vu par drupal ");
     }
   }
   
